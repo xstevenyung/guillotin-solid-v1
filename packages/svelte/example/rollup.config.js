31ca0b65c59c95d4b75705d23d2b883efd4053b3
Steven Yung <stevenyung@fastmail.com>
2021-05-29 22:21:22 +0200

    (example) remove typescript support

--- packages/svelte/example/rollup.config.js
+++ packages/svelte/example/rollup.config.js
@@ -4,7 +4,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
-import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -35,7 +34,7 @@ function serve() {
 }
 
 export default {
-  input: 'src/main.ts',
+  input: 'src/main.js',
   output: {
     sourcemap: true,
     format: 'iife',
@@ -68,10 +67,10 @@ export default {
 
     commonjs(),
 
-    typescript({
-      sourceMap: !production,
-      inlineSources: !production,
-    }),
+    // typescript({
+    //   sourceMap: !production,
+    //   inlineSources: !production,
+    // }),
 
     // In dev mode, call `npm run start` once
     // the bundle has been generated
