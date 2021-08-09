c2daf1fab8cffaf88cbf236a964009a647fe70a3
Steven Yung <stevenyung@fastmail.com>
2021-08-09 15:11:09 +0200

    (ui) upgrade @guillotin/solid deps & remove rounding on container

--- ui/src/components/Container.tsx
+++ ui/src/components/Container.tsx
@@ -2,7 +2,7 @@ import type { Component } from 'solid-js';
 
 const Container: Component = (props) => {
   return (
-    <div class="w-screen h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-2xl flex justify-center items-center">
+    <div class="w-screen h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 flex justify-center items-center">
       {props.children}
     </div>
   );
